9c6d5d37def3db94ffcfb0baab2bae5cd11e5412
shubhamgitvns <shubhammaurya10092002@gmail.com>
2026-06-09 14:33:58 +0530

    Create second bank exapmle in encapsulation

--- encapsulation.py
+++ encapsulation.py
@@ -1,22 +1,17 @@
 
 # incapsulation = class + datahiding + controll access
-class Student:
-    def __init__(self, name, marks):
+class Bank:
+    def __init__(self, name, balance):
         self.name = name
-        self.__marks = marks
+        self.__balance = balance
 
-    def get_marks(self):
-        return self.__marks
+    def deposite(self,amount):
+        self.__balance += amount    
 
-    def set_marks(self, marks):
-        if marks >= 0 and marks <= 100:
-            self.__marks = marks
-        else:
-            print("Invalid marks")
+    def show_balance(self):
+        return self.__balance
 
-s1 = Student("Ravi", 65)
-
-print(s1.name)
-print(s1.get_marks())
-s1.set_marks(95)
-print(s1.get_marks())
+b = Bank("shubham",200)
+b.deposite(500)
+print(b.name)
+print(b.show_balance())
