8694310e91b5946656b402751c25eeebbef653eb
shubhamgitvns <shubhammaurya10092002@gmail.com>
2026-06-09 15:05:56 +0530

    create basic bank-account project with the help of incapsulation

--- encapsulation.py
+++ encapsulation.py
@@ -1,17 +1,30 @@
 
-# incapsulation = class + datahiding + controll access
-class Bank:
+# incapsulation = class + data hiding + controll access
+class BankAccount:
+    # constructer
     def __init__(self, name, balance):
         self.name = name
-        self.__balance = balance
+        self.__balance = balance #private data 
+    
+    # deposite method
+    def deposit(self,amount):
+        self.__balance += amount
+    # deposite method
+    def withdraw(self,amount):
+        if amount > self.__balance:
+            print("Amount is not availabel")
+        else:
+            self.__balance -= amount    
 
-    def deposite(self,amount):
-        self.__balance += amount    
-
+    # show the current balance
     def show_balance(self):
         return self.__balance
-
-b = Bank("shubham",200)
-b.deposite(500)
+    def __str__(self):
+        return f"Name={self.name}, balance={self.__balance}"
+b = BankAccount("shubham",200)
+b.deposit(-8500)
 print(b.name)
 print(b.show_balance())
+b.withdraw(200)
+print(b.show_balance())
+print(b)
